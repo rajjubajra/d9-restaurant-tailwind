d33a007c25834bceb2a30281250920785ef476b9
rajjubajra <rajjubajra@gmail.com>
2021-08-01 11:46:25 +0100

    js file attached

--- js/theme-one/mainmenu.js
+++ js/theme-one/mainmenu.js
@@ -32,7 +32,7 @@ mainmenu.getMainMenu()
   data.map(item=>{
       const {attributes:{title, url}} = item;
       menuItem += `<li>
-        <a href="${theme}/${url}">${title}</a>
+        <a class="p-1 m-1" href="${theme}/${url}">${title}</a>
       </li>`;
   })
 
