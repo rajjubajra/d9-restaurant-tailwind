ccd2dbcb9ad69770ea4584b7a8627d55e935a840
rajjubajra <rajjubajra@gmail.com>
2021-08-02 09:12:52 +0100

    menu main

--- js/theme-one/homepage.js
+++ js/theme-one/homepage.js
@@ -32,7 +32,14 @@ homepage.getHomePage()
   console.log(data.data[0]);
   /** main title */
   document.getElementById('main-title').innerHTML = `${data.data[0].attributes.field_home_block_1_text.value}`;
-
+  /** BLOCK TWO */
   document.getElementById('block-two').innerHTML = `${data.data[0].attributes.field_home_block_2_text.value}`
+
+  /** BLOCK 3 COL 1 */
+  document.getElementById('block-3-col-1').innerHTML = `${data.data[0].attributes.feild_home_block_3_col_1_text}`
+  /** BLOCK 3 COL 2 */
+  document.getElementById('block-3-col-2').innerHTML = `${data.data[0].attributes.feild_home_block_3_col_2_text}`
+  /** BLOCK 3 COL 3 */
+  document.getElementById('block-3-col-3').innerHTML = `${data.data[0].attributes.feild_home_block_3_col_3_text}`
   
 })
