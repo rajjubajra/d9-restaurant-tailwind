f9a853a47b49f09a03d9d928cea0e9eb11814061
rajjubajra <rajjubajra@gmail.com>
2021-08-02 20:56:30 +0100

    reset tailwind css

---
+++ js/theme-one/aboutpage.js
@@ -0,0 +1,27 @@
+class AboutPage{
+  constructor(){
+    this.url = `d9-restaurant/jsonapi/node/about_page`;
+
+  }
+
+
+  async getAboutPage(){
+    const response_data = await fetch(this.url,{
+      method: 'GET',
+      headers:{
+        Accept:'Application/vnd.api+json'
+      }
+    })
+
+    const respones = await response_data.json();
+    return respones;
+  }
+
+}
+
+/** UI */
+const aboutpage = new AboutPage;
+aboutpage.getAboutPage()
+.then(data => {
+  console.log(data);
+})
