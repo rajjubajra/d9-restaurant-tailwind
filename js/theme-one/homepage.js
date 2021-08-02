ada41dc9a1ca8bb7b58dfcffe5fb99f7de2be2ba
rajjubajra <rajjubajra@gmail.com>
2021-08-02 09:43:35 +0100

    menu main

--- js/theme-one/homepage.js
+++ js/theme-one/homepage.js
@@ -36,10 +36,10 @@ homepage.getHomePage()
   document.getElementById('block-two').innerHTML = `${data.data[0].attributes.field_home_block_2_text.value}`
 
   /** BLOCK 3 COL 1 */
-  document.getElementById('block-3-col-1').innerHTML = `${data.data[0].attributes.field_home_block_3_col_1_text}`
+  document.getElementById('block-3-col-1').innerHTML = `${data.data[0].attributes.field_home_block_3_col_1_text.value}`
   /** BLOCK 3 COL 2 */
-  document.getElementById('block-3-col-2').innerHTML = `${data.data[0].attributes.field_home_block_3_col_2_text}`
+  document.getElementById('block-3-col-2').innerHTML = `${data.data[0].attributes.field_home_block_3_col_2_text.value}`
   /** BLOCK 3 COL 3 */
-  document.getElementById('block-3-col-3').innerHTML = `${data.data[0].attributes.field_home_block_3_col_3_text}`
+  document.getElementById('block-3-col-3').innerHTML = `${data.data[0].attributes.field_home_block_3_col_3_text.value}`
   
 })
