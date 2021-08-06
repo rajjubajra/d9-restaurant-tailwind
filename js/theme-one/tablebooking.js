ed1a52d2bd4593309e0eac2a22d4f561a380f101
rajjubajra <rajjubajra@gmail.com>
2021-08-06 17:25:23 +0100

    TABLE BOOKING PAGE

--- js/theme-one/tablebooking.js
+++ js/theme-one/tablebooking.js
@@ -18,36 +18,7 @@ class TableBooking{
 const tablebooking = new TableBooking;
 
 
-const newNode = {
-  _links: {
-    type: {
-      href: 'http://example.com/rest/type/node/article'
-    }
-  },
-  type: {
-    target_id: 'article'
-  },
-  title: {
-    value: 'Example node title'
-  }
-}; 
 
-
-const bookingForm = document.getElementById("booking-form");
-
-bookingForm.addEventListener('submit', function(e){
-  e.preventDefault();
-
-  const formData = new FormData(this);
-  
-  fetch(`/d9-restaurant/jsonapi/node/artilce`,{
-    method: 'POST',
-    headers:{
-      Accept: 'application/vnd.api+json'
-    },
-    data: JSON.stringify(newNode)
-  }).then(res => {    
-    console.log("POSTED DATA FORM DATA",res.json);
-  })
-
-})
+fetch('/d9-restaurant/session/token')
+  .then(response => console.log(response.json()))
+  .then(data => console.log(data));
