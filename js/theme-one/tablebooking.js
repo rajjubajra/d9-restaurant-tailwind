7e8a4847943c378d4adb03a73fa700c9585e9f19
rajjubajra <rajjubajra@gmail.com>
2021-08-08 13:09:35 +0100

    axios js added

--- js/theme-one/tablebooking.js
+++ js/theme-one/tablebooking.js
@@ -18,7 +18,7 @@ class TableBooking{
       })
       const csrf_token = response.data
       singleton = axios.create({
-        baseURL: config.drupal_url, // every request is relative to this URL
+        baseURL: `rdbajra.com/d9-restaurant`, // every request is relative to this URL
         withCredentials: true, // include auth cookie in every request
         headers: { 'X-CSRF-Token': csrf_token }, // include this header in every request
         params: { _format: 'json' }, // add these query params to every request
